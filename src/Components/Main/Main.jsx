cf05d0f62448c2260c2cefb134830af36fded4b5
farrux <orziyevfarrux01@gmail.com>
2023-09-24 18:16:02 +0500

    fixed path

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Vacancy from "../Vacancy/Vacancy";
-import Tourism from "../Tourism/Tourismo";
+import Tourism from "../Tourism/Tourism";
 import Education from "../Education/Education";
 import "./main.scss";
 
